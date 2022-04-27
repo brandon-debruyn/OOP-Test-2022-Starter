076ad26888951e3b9e05b237881c2d4e598fce80
brandon-debruyn <brandondebruyn123@gmail.com>
2022-04-27 17:37:11 +0100

    added limbs + eyes

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -27,45 +27,38 @@ public class Nematode {
         this.eyes = eyes;
     }
 
+    
     public String getNematodeName() {
         return nematodeName;
     }
-
     public void setNematodeName(String nematodeName) {
         this.nematodeName = nematodeName;
     }
-
     public int getLength() {
         return length;
     }
-
     public void setLength(int length) {
         this.length = length;
     }
-
     public boolean isLimbs() {
         return limbs;
     }
-
     public void setLimbs(boolean limbs) {
         this.limbs = limbs;
     }
-
     public String getGender() {
         return gender;
     }
-
     public void setGender(String gender) {
         this.gender = gender;
     }
-
     public boolean isEyes() {
         return eyes;
     }
-
     public void setEyes(boolean eyes) {
         this.eyes = eyes;
     }
+
 
     @Override
     public String toString() {
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -2,6 +2,8 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
+import javax.swing.border.LineBorder;
+
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
@@ -29,6 +31,8 @@ public class NematodeVisualiser extends PApplet
 		colorMode(HSB);
 		background(0);
 		smooth();		
+		
+		// check load -> with toString Method
 		loadNematodes();
 		for( Nematode n:nematodes) {
 			System.out.println(n);
@@ -45,8 +49,61 @@ public class NematodeVisualiser extends PApplet
 		}
 	}
 
+	int choice = 0;
+	float radius = 75.0f;
+	float limbLength = 5.0f;
+	float eyeSize = 10;
 
-	public void draw()
-	{	
+	public void displayNematodes() {
+		background(0);
+		float halfW = width / 2;
+		float boundary = 0.15f * width;
+		
+
+		float c = map(choice, 1, nematodes.size(), 0, 255);
+		
+		fill(c, 255, 255);
+		textSize(35);
+		textAlign(CENTER, LEFT);
+		text(nematodes.get(choice).getNematodeName(), halfW, boundary);
+
+		stroke(c, 255, 255);
+		strokeWeight(5);
+		noFill();
+		for(int i=0; i<nematodes.get(choice).getLength(); i++) {
+			
+			ellipse(halfW, (2 * boundary) + ((i) * radius) , radius, radius);
+			
+			stroke(c, 255, 255);
+			if(nematodes.get(choice).isLimbs()) {
+				line(halfW + radius / 2, (2 * boundary) + ((i) * radius), halfW  + radius + limbLength, (2 * boundary) + ((i) * radius));
+				line(halfW - radius / 2, (2 * boundary) + ((i) * radius), halfW - radius - limbLength, (2 * boundary) + ((i) * radius));
+			}
+
+		}
+
+		if(nematodes.get(choice).isEyes()) {
+
+			noFill();
+			float x = halfW  + sin(PI / 4) * radius;
+			float y = (2 * boundary) - cos(PI /4) * radius;
+			line(x, y , halfW + radius / 2 , y + radius / 2);
+
+			circle(x, y, eyeSize);
+			
+			x = halfW  + sin(3 * PI / 4) * -radius;
+			y = (2 * boundary) - cos(3 * PI /4) * -radius;
+			line(x , y, halfW - radius / 2, y + radius / 2);
+			circle(x, y, eyeSize);
+
+		}
+			
+	}
+
+	
+	public void draw() {
+		background(0);
+		displayNematodes();
+		
 	}
 }
